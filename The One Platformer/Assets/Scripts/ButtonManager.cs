32a2a6f6847f8e5a7ebcad7d642aff00fee1391e
Raul Montes <montesraul7@gmail.com>
2020-09-13 16:42:08 -0700

    Some changes.

---
+++ The One Platformer/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ResetLevelGM()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+}
